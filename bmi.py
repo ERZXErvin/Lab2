7ee46f34e874a2e2662c2f9f34cc767edbba37b8
ERZXErvin <ERZX.23@ichat.sp.edu.sg>
2024-04-22 10:19:10 +0800

    bmi.py

---
+++ bmi.py
@@ -0,0 +1,24 @@
+bmi = 0
+weight = 0
+height = 0
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+  
+ #Add code here to calculate BMI
+    bmi = weight/(height*height)
+ 
+ #Add code here to display calculate BMI
+    print("BMI = " + str(bmi))
+ 
+    if (bmi < 18.5):
+        print ("Underweight")
+
+    if (bmi >= 18.5, bmi <= 25.0):
+        print ("Normal Weight")
+
+    if (bmi > 25.0):
+        print ("Overweight")
+    
+calculate_bmi(weight=57, height=1.73)
+
