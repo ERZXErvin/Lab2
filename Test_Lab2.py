fedc8a7d4ea0c06aa2ee99a7da194074948747e1
ERZXErvin <ERZX.23@ichat.sp.edu.sg>
2024-05-01 22:20:10 +0800

    Lab2Test

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -11,10 +11,10 @@ def test_calc_average():
     result = []
     user_input =  [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ]
     result = [ 5.5 ]
-    assert(Lab2.calc_average_temperature(user_input))
+    assert(result == Lab2.calc_average_temperature(user_input))
 
 def test_calc_median_temperature():
     result = []
     user_input =  [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ]
     result = [ 5.5 ]
-    assert(Lab2.calc_median_temperature(user_input))
+    assert(result == Lab2.calc_median_temperature(user_input))
